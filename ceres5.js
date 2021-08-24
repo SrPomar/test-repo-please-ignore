6d6b6a29b4ad784e9e1aa39d09aa8469e473f037
Andreu Pomar <77430921+SrPomar@users.noreply.github.com>
2021-08-24 12:09:20 +0200

    Add files via upload

---
+++ ceres5.js
@@ -0,0 +1,19 @@
+var lienzo = "https://i.imgur.com/m4Wg23l.png"
+
+
+var lugares = [
+    new Lugar ("Puerto de mercancías de Ceres", m.none, "Puertos", "red",[[1123, 565]],"", "El principal punto de entrada y salida de mercancías de Ceres. Actualmente los envíos proceden con lentitud debido a la huelga"),
+    new Lugar ("Puerto espacial Este", m.none, "Puertos", "red",[[1035, 384]],"", "El destino de aquellos desafortunados que se han visto obligados a viajar hasta Ceres para trabajar."),
+    new Lugar ("Puerto espacial corporativo", m.none, "Puertos", "red",[[768, 779]],"", "Un pequeño hangar construido principalmente para alojar los yates que transportan ejecutivos y personal corporativo hasta la estación."),
+    new Lugar ("Cúpula de habitaculos Alfa", m.none, "Habitáculos", "blue",[[1180, 634]],"", "La primera y más antigua cúpula de habitáculos en Ceres. Actualmente los niveles aéreos están prácticamente en desuso ya que toda la actividad se desarrolla en sus expansiones subterráneas."),
+    new Lugar ("Cúpula de habitáculos Beta", m.none, "Habitáculos", "blue",[[1010, 459]],"", "La expansión más reciente para alojar al creciente número de trabajadores hacinados en Ceres. Ligeramente menos opresiva que la cúpula Alfa, pero sigue siendo un ejemplo de manual de la arquitectura brutalista espacial."),
+    new Lugar ("Domo corporativo", m.none, "Habitáculos", "red",[[760, 687]],"", "Esta megaestructura combina las viviendas de los trabajadores corporativos, las sucursales en las que trabajan y una zona de ocio, que incluye un enorme parque bajo la cúpula de cristal."),
+    new Lugar ("Complejo científico Whitestar", m.none, "Industria", "green",[[873, 773]],"", "Un enorme laboratorio multidisciplinario de la compañía Whitestar, utilizado para proyectos de investigación especiales."),
+    new Lugar ("Cúpula hidropónica Cosmic Farms", m.none, "Industria", "green",[[1101, 434]],"", "La principal fuente de comida del planeta enano, así como de aire limpio. Obviamente, Cosmic Farms cobra un precio desorbitado por el lujo de poder respirar."),
+    new Lugar ("Distrito industrial", m.none, "Industria", "green",[[1224, 537]],"", "Una enorme colección de refinerías y fábricas que se encargan de convertir los asteroides en materias útiles y productos listos para ser exportados a los mundos interiores. Debido a la huelga, la producción se ha visto afectada."),
+    new Lugar ("Pozo de minería planetaria 1", m.none, "Industria", "green",[[1395, 675]],"", ""),
+    new Lugar ("Pozo de minería planetaria 2", m.none, "Industria", "green",[[1339, 425]],"", ""),
+
+    new Lugar ("Antiguo complejo CLM", m.none, "Ruinas", "blue", [[522, 606]], "Nadie habla del complejo de Ceres Light Manufacturing.", ""),
+    new Lugar ("Fábrica superpesada de Athena", m.none, "Expansiones futuras", "blue", [[461, 554]], "Nadie habla del complejo de Ceres Light Manufacturing.", ""),
+]
